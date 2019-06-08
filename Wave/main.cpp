a5cbf35e1eb20db96e1db3a6d9b81b7701b45601
Arthur Sun <arthursun2005@gmail.com>
2019-06-08 14:20:37 +1000

    Initial Commit

---
+++ Wave/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wave
+//
+//  Created by Arthur Sun on 6/8/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
